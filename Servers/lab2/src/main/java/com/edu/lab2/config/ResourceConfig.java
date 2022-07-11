1d8242d77b9be25279d46d989e9c3676a1f9af56
To Minh Tri <balisongian@gmail.com>
2022-07-11 21:35:49 +0700

    sa

--- Servers/lab2/src/main/java/com/edu/lab2/config/ResourceConfig.java
+++ Servers/lab2/src/main/java/com/edu/lab2/config/ResourceConfig.java
@@ -3,15 +3,18 @@ package com.edu.lab2.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class ResourceConfig {
-    @Bean("messageSource")
+    @Bean
+    @Description("Spring Message Resolver")
     public MessageSource getMessageSource() {
-        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
         ms.setDefaultEncoding("utf-8");
-        ms.setBasename("classpath:menu");
+        ms.setBasename("messages");
         return ms;
     }
 
