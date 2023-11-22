5b19e8226536e7b2317563f6241e91c1a13d687d
jt <balisongian@gmail.com>
2023-11-22 13:38:12 +0700

    push

--- learn-algo-solving/src/main/java/org/example/Algo.java
+++ learn-algo-solving/src/main/java/org/example/Algo.java
@@ -6,29 +6,67 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 public class Algo {
 
     public static void main(String[] args) {
-        System.out.println(solution1("BBABAA"));
     }
 
-    // We are given a string S of length N consisting only of letters 'A' and/or
-    // 'B'. Our goal is to obtain a string in the format "A...AB...B" (all letters
-    // 'A' occur before all letters 'B') by deleting some letters from S. In
-    // particular, strings consisting only of letters 'A' or only of letters 'B' fit
-    // this format.
-    // Write a function:
-    // class Solution { public int solution (String s); }
-    // that, given a string S, returns the minimum number of letters that need to be
-    // deleted from S in order to obtain a string in the above format.
-    // Examples:
-    // 1. Given S = "BAAABAB", the function should return 2. We can obtain "AAABB"
-    // by deleting the first occurrence of 'B' and the last occurrence of 'A'.
-    // 2. Given S = "BBABAA", the function should return 3. We can delete all
-    // occurrences of 'A' or all occurrences of 'B'.
-    // 3. Given S = "AABBBB", the function should return 0. We do not have to delete
-    // any letters, because the given string is already in the expected format.
-    // Write an efficient algorithm for the following assumptions:
-    // • N is an integer within the range [1..100,000];
-    // ⚫ string S is made only of the characters 'A' and/or 'B'.
-                
+    // Codility Draw Table
+    public static String codilityDrawTable(int[] A, int K, int index, String table) {
+        if (A == null || A.length == 0 || K <= 0) {
+            return "Invalid input";
+        }
+        int longest = getLongest(A);
+        if (index >= A.length) {
+            int widthLength = K;
+            boolean isLastCell = (index - 1) % K == K - 1;
+            boolean isJustNormalCellNotLast = (index % K < K - 1) && (index % K != 0);
+            if (isJustNormalCellNotLast) {
+                table += "\n";
+            }
+            table += "+";
+            if (!isLastCell) {
+                widthLength = index % K;
+            }
+            for (int j = 0; j < widthLength; j++) {
+                for (int k = 0; k < longest; k++) {
+                    table += "-";
+                }
+                table += "+";
+            }
+            table += "\n";
+            return table;
+        }
+        if (index % K == 0) {
+            table += "+";
+            int width = A.length / K == 0 ? A.length % K : K;
+            for (int j = 0; j < width; j++) {
+                for (int k = 0; k < longest; k++) {
+                    table += "-";
+                }
+                table += "+";
+            }
+            table += "\n";
+            table += "|";
+        }
+        table += String.format("%" + longest + "d|", A[index]);
+        if (index % K == K - 1) {
+            table += "\n";
+        }
+        return codilityDrawTable(A, K, index + 1, table);
+    }
+
+    public static int getDigits(int n) {
+        if (n < 10) {
+            return 1;
+        }
+        return 1 + getDigits(n / 10);
+    }
+
+    public static int getLongest(int[] A) {
+        int longest = 0;
+        for (int n : A) {
+            longest = Math.max(longest, getDigits(n));
+        }
+        return longest;
+    }
 
     // https://leetcode.com/problems/contains-duplicate-ii/
     static boolean containsNearbyDuplicate(int[] nums, int k) {
