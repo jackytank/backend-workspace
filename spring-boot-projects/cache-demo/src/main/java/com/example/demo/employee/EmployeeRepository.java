f782d30289b54a6bc8f6431c78ac993acc27ed91
jt <balisongian@gmail.com>
2023-11-30 13:11:18 +0700

    add @Query

--- spring-boot-projects/cache-demo/src/main/java/com/example/demo/employee/EmployeeRepository.java
+++ spring-boot-projects/cache-demo/src/main/java/com/example/demo/employee/EmployeeRepository.java
@@ -6,8 +6,16 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // find Optioanl Employee by email or name or phone by using @Query,
+    // use only 1 @Param keyword
+    @Query("SELECT e FROM Employee e WHERE e.email = :email OR e.name = :name OR e.phone = :phone")
+    Optional<Employee> findByEmailOrNameOrPhone(
+            @Param("email") String email,
+            @Param("name") String name,
+            @Param("phone") String phone);
 
     @Query("SELECT e FROM Employee e")
     List<Employee> search(Pageable pageable);
