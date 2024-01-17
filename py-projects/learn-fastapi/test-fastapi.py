02091b5b6e34770be8b1a51f732c518bdd2897b1
Tri Minh To <tri.minhto@hitachivantara.com>
2024-01-17 09:36:21 +0700

    finish quick fastapi tut

--- py-projects/learn-fastapi/test-fastapi.py
+++ py-projects/learn-fastapi/test-fastapi.py
@@ -17,6 +17,12 @@ class Student(BaseModel):
     year: str
 
 
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
+
 @app.get("/")
 def index():
     return {"name": "First data"}
@@ -41,3 +47,24 @@ def create_student(student_id: int, student: Student):
         return {"Error": "Student exists"}
     students[student_id] = student
     return students[student_id]
+
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    if student.name is not None:
+        students[student_id].name = student.name
+    if student.age is not None:
+        students[student_id].age = student.age
+    if student.year is not None:
+        students[student_id].year = student.year
+    students[student_id] = student
+    return students[student_id]
+
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    del students[student_id]
